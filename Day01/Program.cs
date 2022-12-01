35648e02ef677ff6c9435452c00100ba7ca495e7
Tobias Sonderbo <tobias.l.sonderbo@gmail.com>
2022-12-01 17:35:43 +0100

    Day01

---
+++ Day01/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Day01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string values = File.ReadAllText(@"C:\Repos\Advent of code\Day1\Day1\input.txt");
+            string[] splitValues = values.Split(new[] { "\n\n" }, StringSplitOptions.None);
+
+            var list = splitValues.Select(line => line.Split(new[] { "\n" }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Sum())
+                .OrderByDescending(x => x)
+                .ToList();
+
+            Console.WriteLine("Biggest number: " + list.First());
+            Console.WriteLine("Biggest 3 numbers sum: " + (list.Take(3).Sum()));
+        }
+    }
+}
